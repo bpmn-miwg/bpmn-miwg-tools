973f4caf482d3dba9fbe766d4c212f8ac417c94e
Tim Stephenson <tim@thestephensons.me.uk>
2013-08-02 18:14:20 +0100

    Performance optimisation (cached syntax highlighted BPMN). Unsuccessful investigation of xpath selectors. Improve separation of code and view

--- src/main/webapp/js/bpmn-miwg.js
+++ src/main/webapp/js/bpmn-miwg.js
@@ -26,16 +26,47 @@
 var mi = new ModelInterchangePresenter();
 
 function ModelInterchangePresenter() {
+	this.orig = [];
+	this.init = function() {
+	  var variants = ['reference','vendor'];
+	  $.each(variants, function(i,d) {
+	      mi.loadBpmn(d);
+	      // register highlight click handlers for xpath findings
+	      $('.finding span.'+d+'[data-xpath]').append('&nbsp;<a class="'+d+'">Show in '+d+'</a>').click(function(){
+	    	 console.log('finding clicked'); 
+	    	 mi.scrollToXPath($(this).parent().parent().parent().data('test'),$(this).parent().data('xpath'),d);
+	    	 return false;
+	      }); 
+	      // register highlight click handlers for string findings
+	      $('.finding span.'+d+'[data-frag]').append('&nbsp;<a class="'+d+'">Show in '+d+'</a>').click(function(){
+	    	 console.log('finding clicked'); 
+	    	 mi.scrollToFrag($(this).parent().parent().parent().data('test'),$(this).data('frag'),d);
+	    	 return false;
+	      }); 
+	  });
+  }
   this.reset = function() {
     $('#highlight-reference').removeClass('highlight');
     $('#highlight-vendor').removeClass('highlight');
-    if (orig.length == ($('code.reference').length*2)) {
-      for (idx in orig) {
-        $( 'div[data-test="'+orig[idx].test+'"] code.'+orig[idx].target ).empty().html( orig[idx].data );
-        $('pre code').each(function(i, e) { hljs.highlightBlock(e) });
+    if (mi.orig.length == ($('code.reference').length*2)) {
+      console.log('All models fetched, now loading...');
+      for (idx in mi.orig) {
+        if (mi.orig[idx].html == null) {
+        	$( 'div[data-test="'+mi.orig[idx].test+'"] code.'+mi.orig[idx].target )
+        	.empty().html( mi.orig[idx].data );
+	        $('pre code').each(function(i, d) { 
+	        	hljs.highlightBlock(d);
+	        	mi.orig[idx].html = $( 'div[data-test="'+mi.orig[idx].test+'"] code.'+mi.orig[idx].target ).html();
+	        	console.log('added syntax highlighting: '+ mi.orig[idx].html.length);
+	        });
+        } else {
+        	console.log('Already applied syntax highlighting'); 
+        	$( 'div[data-test="'+mi.orig[idx].test+'"] code.'+mi.orig[idx].target )
+        		.empty().html( mi.orig[idx].html );
+        }
       }
     } else {
-      console.log('waiting for all models to be loaded');
+      console.log('Loaded '+mi.orig.length+' of '+$('code.reference').length*2+', waiting for all models to be fetched');
     }
   };
   this.loadBpmn = function(target) {
@@ -48,8 +79,14 @@ function ModelInterchangePresenter() {
         dataType: 'text',
         success: function( data ) {
           console.log('success loading '+file+' for '+test);
-          var xml = data.replace(/</g,'&lt;');
-          orig.push({"target":target,"test":test,"data":xml});
+          var o = {
+        	  "target":target,
+        	  "test":test,
+        	  "data":data.replace(/</g,'&lt;'),
+        	  "html":null,
+        	  "xml":$.parseXML(data)
+          };
+          mi.orig.push(o);
           mi.reset();
         },
         error: function(jqXHR, textStatus, errorThrown) {
@@ -58,16 +95,43 @@ function ModelInterchangePresenter() {
       });
     });
   };
-  this.scrollToFrag = function(test, idx, target) {
-    this.reset();    
-    this.highlightFrag(test, idx,'reference');
-    this.highlightFrag(test, idx,'vendor');
-    document.getElementById('highlight-'+target).scrollIntoView(false); 
+  this.scrollToXPath = function(test, idxOrFrag, target) {
+	  // NOTE THAT THIS DOES NOT WORK!! SUGGESTIONS WELCOMED
+	  
+	  // make a local copy of the first xml document loaded to play with 
+	  var x = mi.orig[0].xml;
+	  
+	  // create a namespace resolver 
+	  var nsResolver = x.createNSResolver( x );
+	  
+	  // both of these return root === undefined
+	  //var root = x.evaluate('//semantic:definitions',x,nsResolver ,XPathResult.ANY_TYPE,null);
+	  var root = x.evaluate('/',x,nsResolver ,XPathResult.ANY_TYPE,null);
+	 
+	  
   };
-  this.highlightFrag= function(test,fragIdx,target) {
-    var fragToSeek = frags[fragIdx];
-    console.log('Highlighting: '+ fragToSeek+' in '+target);
-    var xml = $( 'div[data-test="'+test+'"] code.'+target ).html();
+  this.scrollToFrag = function(test, idxOrFrag, target) {
+	this.showActivityIndicator('Searching...');
+    this.reset();    
+    var frag = idxOrFrag;
+    //if (!isNaN(idxOrFrag)) frag = frags[idxOrFrag];
+    var refRtn = this.highlightFrag(test,frag,'reference');
+    var vendorRtn = rtn = this.highlightFrag(test,frag,'vendor');
+    if (target=='reference' && refRtn != -1) {
+    	document.getElementById('highlight-'+target).scrollIntoView(false); 
+    } else if (target=='vendor' && vendorRtn != -1) { 
+    	document.getElementById('highlight-'+target).scrollIntoView(false); 
+    }
+    this.hideActivityIndicator();
+  };
+  this.highlightFrag= function(test,fragToSeek,target) {
+	console.log('Highlighting: '+ fragToSeek+' in '+test+'.'+target);
+	if (fragToSeek===undefined) {
+		console.error('No fragment specified');
+		return -1;
+	}
+    var el = $( 'div[data-test="'+test+'"] code.'+target ); 
+    var xml = el.html();
     // adjust string to seek for the code highlighting 
     // e.g. id="11" becomes 
     //      <span class="attribute">id</span></span>=<span class="value"><span class="value">"11
@@ -75,14 +139,25 @@ function ModelInterchangePresenter() {
     
     var idx = xml.indexOf(fragToSeek);
     if (idx==-1) {
-      $('li[data-index='+fragIdx+'] .'+target).replaceWith('<span id="highlight-'+target+'" class="highlight">Not found</span>');
+      console.log('Frag not found: '+fragToSeek);
+      $('a[data-frag="'+fragToSeek+'"] .'+target).replaceWith('<span id="highlight-'+target+'" class="highlight">Not found</span>');
     } else { 
       console.log('Found frag at: '+ idx);
       var fragBefore = xml.substring(0,idx);
       //console.log('Frag before: '+fragBefore.substring(0,20)+'...'+fragBefore.substring(fragBefore.length-20));
       var fragAfter = xml.substring(idx+fragToSeek.length);
       //console.log('Frag after: '+fragAfter.substring(0,20)+'...'+fragAfter.substring(fragBefore.length-20));
-      $( '#'+target ).html(fragBefore+'<span id="highlight-'+target+'" class="highlight">'+fragToSeek+'</span>'+fragAfter);
+      el.html(fragBefore+'<span id="highlight-'+target+'" class="highlight">'+fragToSeek+'</span>'+fragAfter);
     }
+    return idx; 
+  }
+  this.showActivityIndicator = function(msg) {
+	  document.body.style.cursor='progress';  
+	  if (msg !== undefined) msg = 'Working...';
+	  $('.messages').empty().append(msg).removeClass('hide').addClass('blink');
+  }
+  this.hideActivityIndicator = function() {
+	  $('.messages').addClass('hide');
+	  document.body.style.cursor='auto';  
   }
 }
