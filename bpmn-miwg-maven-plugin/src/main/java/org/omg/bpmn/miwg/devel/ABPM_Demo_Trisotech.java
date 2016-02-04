1158b9174cf29ecdbb08f38a8d08f875937786c1
MK <mk@local>
2016-02-04 16:12:39 +0100

    ABPM-Demo

--- bpmn-miwg-maven-plugin/src/main/java/org/omg/bpmn/miwg/devel/ABPM_Demo_Trisotech.java
+++ bpmn-miwg-maven-plugin/src/main/java/org/omg/bpmn/miwg/devel/ABPM_Demo_Trisotech.java
@@ -47,7 +47,7 @@ public class ABPM_Demo_Trisotech extends AbstractTest {
 	public static List<Object[]> data() throws IOException, ReferenceNotFoundException {
 		return BpmnFileScanner
 				.data(new StandardScanParameters(
-						"Trisotech BPMN Modeler",
+						"Trisotech BPMN Modeler 5.0.15",
 						null));
 	}
 
